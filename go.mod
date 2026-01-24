774755ee1b0c9143a178b3fa662e55ad50a8ce05
Takayuki Miyahara <voyager.3taka28@gmail.com>
2026-01-25 02:48:57 +0900

    feat(all): initialize raft project structure
    - Create go.mod for module github.com/taka/raft-sample
    - Add directory structure: raft/, transport/, proto/, cmd/server/
    - Add placeholder main.go
    - Add .gitignore for binaries and IDE files

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/taka/raft-sample
+
+go 1.25.4
---
+++ main.go
@@ -0,0 +1,7 @@
+// Package main は Raft サーバーのエントリポイントです。
+// 学習目的で Raft コンセンサスアルゴリズムを実装しています。
+package main
+
+func main() {
+	// TODO: Raft サーバーの起動処理を実装
+}
